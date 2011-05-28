c750702f9ff52695d171db3592c8c3d26ffbd061
Karl Danninger <kdanninger@gmail.com>
2011-05-28 19:47:51 -0400

    added modularized code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,9 @@ module ApplicationHelper
     else
       "#{base_title} | #{@title}"
     end
+
   end
+      def logo
+        image_tag("logo.png", :alt => "Sample App", :class => "round")
+      end
 end
