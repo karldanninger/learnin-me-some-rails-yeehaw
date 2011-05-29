ace2fc53376c1c33d9131380ec6ff167062f4e3c
Karl Danninger <kdanninger@gmail.com>
2011-05-29 16:46:56 -0400

    Improved the user show page and added a gravatar

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Karl Danninger"
+  user.email                 "kdanninger@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
