2a0a1fb4628f7822a28111e0b7fa15194617bfdc
Karl Danninger <kdanninger@gmail.com>
2011-05-29 02:34:45 -0400

    Added a stub user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
   
-  email_regex = /\A[w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name,  :presence => true,
                     :length   => { :maximum => 50 }
