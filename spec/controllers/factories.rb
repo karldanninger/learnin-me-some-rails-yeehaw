5d2fb4fcca8d942529f6676443986af610ac5305
Karl Danninger <kdanninger@gmail.com>
2011-05-29 18:00:06 -0400

    Added a signup form

--- spec/controllers/factories.rb
+++
@@ -1,6 +0,0 @@
-Factory.define :user do |user|
-  user.name                  "Karl Danninger"
-  user.email                 "kdanninger@example.com"
-  user.password              "foobar"
-  user.password_confirmation "foobar"
-end
