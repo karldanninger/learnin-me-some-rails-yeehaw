715ab2529b245593ea53f4a73b0a28cf2f8dbf89
Karl Danninger <kdanninger@gmail.com>
2011-05-30 18:40:12 -0400

    Working user signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      #handle successful signin
+      sign_in user
+      redirect_to user
     end
     
     
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -15,32 +15,46 @@ describe SessionsController do
     end
   end
 
-describe "POST 'create'" do
-  describe "failure" do
+  describe "POST 'create'" do
+    describe "failure" do
     
-    before(:each) do
-      @attr = {:email => "", :password => "" }
+      before(:each) do
+        @attr = {:email => "", :password => "" }
+      end
+    
+      it "should re-render the new page" do
+        post :create, :session => @attr
+        response.should render_template('new') 
+      end
+    
+      it "should have an error message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+    
+      it "should have the right title" do
+        post :create, :session => @attr
+        response.should have_selector('title', :content => "Sign in")
+      end
     end
+  
+    describe "success" do
     
-    it "should re-render the new page" do
-      post :create, :session => @attr
-      response.should render_template('new') 
+      before(:each) do
+        @user = Factory(:user)
+        @attr = { :email => @user.email, :password => @user.password }
+      end
+    
+      it "should sign the user in" do
+        post :create, :session => @attr
+        controller.current_user.should == @user
+        controller.should be_signed_in
+      end
+    
+      it "should redirect to the user show page" do
+        post :create, :session => @attr
+        response.should redirect_to(user_path(@user))
+      end
     end
-    
-    it "should have an error message" do
-      post :create, :session => @attr
-      flash.now[:error].should =~ /invalid/i
-    end
-    
-    it "should have the right title" do
-      post :create, :session => @attr
-      response.should have_selector('title', :content => "Sign in")
-    end
-    
-    
   end
-end
-
-
-
 end
