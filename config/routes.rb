32e57de259945eaa4d0cf79f5b796b02394e28a3
Karl Danninger <kdanninger@gmail.com>
2011-05-30 17:34:39 -0400

    Added a Session controller and a Signin view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 SampleApp::Application.routes.draw do
   
+  
   resources :users  
+  resources :sessions, :only => [:new, :create, :destroy]
 
     root :to => 'pages#home'  
     match '/contact', :to => 'pages#contact'
     match '/about',   :to => 'pages#about'
     match '/help',    :to => 'pages#help'
     match '/signup',  :to => 'users#new'
+    match '/signin',  :to => 'sessions#new'
+    match '/signout', :to => 'sessions#destroy'
   
 
   # The priority is based upon order of creation:
