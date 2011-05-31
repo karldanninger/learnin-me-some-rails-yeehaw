83bede3767880cb7b1d690047a847212ea06416f
Karl Danninger <kdanninger@gmail.com>
2011-05-30 20:11:47 -0400

    Completed signin/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,13 +13,12 @@ class SessionsController < ApplicationController
     else
       sign_in user
       redirect_to user
-    end
-    
-    
-    
+    end  
   end
   
   def destroy
+    sign_out
+    redirect_to root_path
   end
 
 end
